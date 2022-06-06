dc1b6f1c8c1397b144ef854d4d503eed2c0039d0
Muhammad Firdaus Maulana <firdaus452maulana@gmail.com>
2022-06-06 14:54:57 +0700

    Add Login

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -10,22 +10,23 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-  public function register(Request $request) {
+  public function register(Request $request)
+  {
     try {
       $data = $request->validate([
         'name' => 'required|string|max:255',
         'email' => 'required|email|max:255|unique:users,email',
         'password' => 'required|string',
       ]);
-  
+
       $user = User::create([
         'name' => $data['name'],
         'email' => $data['email'],
         'password' => Hash::make($data['password']),
       ]);
-  
+
       $token = $user->createToken('projectToken')->plainTextToken;
-  
+
       return response()->json([
         'code' => 201,
         'message' => 'User Created',
@@ -37,6 +38,32 @@ class AuthController extends Controller
         'code' => 400,
         'message' => $error->getMessage()
       ], 400);
+    }
+  }
+
+  public function login(Request $request)
+  {
+    $data = $request->validate([
+      'email' => 'required|email|max:255',
+      'password' => 'required|string',
+    ]);
+
+    $user = User::where('email', $data['email'])->first();
+
+    if (!$user || !Hash::check($data['password'], $user->password)) {
+      return response()->json([
+        'code' => 401,
+        'message' => 'Invalid Credential',
+      ], 401);
+    } else {
+      $token = $user->createToken('mahasiswaToken')->plainTextToken;
+      $response = [
+        'code' => 200,
+        'user' => $user,
+        'token' => $token
+      ];
+
+      return response()->json($response, 200);
     }
   }
 }
