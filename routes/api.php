1e999573ea447fc1848cbbf2f85848cae42f35d3
Muhammad Firdaus Maulana <firdaus452maulana@gmail.com>
2022-06-16 21:48:35 +0700

    Add Verify Email Route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\EmailVerificationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,7 +18,10 @@ use App\Http\Controllers\API\AuthController;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('email/verification-notification', [EmailVerificationController::class,'sendVerificationEmail']);
+    Route::post('verify-email/{id}/{hash}', [EmailVerificationController::class,'verify'])->name('verification.verify');
 });
